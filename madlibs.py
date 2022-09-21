58bcce73d8109aba1d2c2649d509f2c175459488
Carina Ogbat <carinawrightogbat@gmail.com>
2022-09-21 15:56:40 -0700

    finished regular lab, added some css

--- madlibs.py
+++ madlibs.py
@@ -50,6 +50,22 @@ def greet_person():
 
     return render_template("compliment.html", person=player, compliment=compliment)
 
+@app.route("/game")
+def show_madlib_form():
+    play_game = request.args.get("play-game")
+
+    if play_game == 'yes':
+        return render_template("game.html")
+    return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    person = request.args.get("name")
+    adjective = request.args.get("adj")
+
+    return render_template("madlib.html", color=color, noun=noun, person=person, adjective=adjective)
 
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
